56b37641b0d397054f8b2e638062549019fda4b0
opvasger <opvasger@gmail.com>
2018-07-12 14:37:42 +0200

    now removes node_modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use std::error::Error;
+use std::ffi::OsString;
+use std::fs::remove_dir_all;
+use std::path::Path;
 
 pub struct Config {
     pub root: String,
@@ -16,8 +19,33 @@ impl Config {
     }
 }
 
-pub fn run(_: Config) -> Result<(), Box<Error>> {
-    // recursively run through subdirectories to remove "node_modules"
+fn search_and_destroy(path: &Path) -> Result<(), Box<Error>> {
+    for entry in path.read_dir()? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if !path.is_dir() {
+            continue;
+        }
+
+        if entry.file_name() == OsString::from("node_modules") {
+            let _ = remove_dir_all(&path)?;
+        } else {
+            let _ = search_and_destroy(&path);
+        }
+    }
+
+    Ok(())
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let path = Path::new(&config.root);
+
+    if !path.is_dir() {
+        return Err(From::from(format!("'{}' is not a directory", &config.root)));
+    }
+
+    search_and_destroy(path);
 
     Ok(())
 }
